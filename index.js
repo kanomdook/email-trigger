d46285118cc4ba80ccf1134607d81d3485a2bbf4
kanomdook <kanung.2535@gmail.com>
2019-01-10 11:05:16 +0700

    [edit] api#2

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
+// app.use(express.static(path.join(__dirname, 'dist')));
 
 // app.get('*', (req, res) => {
 //     console.log({
