f26058fa9184c60fa0e676d80ec1bf7970fcc708
kanomdook <kanung.2535@gmail.com>
2019-01-17 09:31:47 +0700

    [edit] work flow

--- work-flow.js
+++ work-flow.js
@@ -1,7 +1,7 @@
 const line = require('./line.js');
 module.exports = function (req, res) {
     console.log('========req==========');
-    console.log(JSON.parse(req.body));
+    console.log(req.body);
     console.log('=====================');
     run(req.body, res);
 }
