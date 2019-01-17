82fa06d3adcb6242f399ba062b31c8a16d7d4b21
kanomdook <kanung.2535@gmail.com>
2019-01-17 10:30:28 +0700

    [edit] token

--- work-flow.js
+++ work-flow.js
@@ -1,20 +1,18 @@
 const line = require('./line.js');
 module.exports = function (req, res) {
-    console.log('========req==========');
-    console.log(req.body);
-    console.log('=====================');
     run(req.body, res);
 }
 
 async function run(params, res) {
     try {
         const tokenRes = await line.getToken(params);
-        if (tokenRes.status === 200) {
+        if (tokenRes.status == 200) {
+            console.log('********have token*******!');
             const lineRes = await line.sendNotification(tokenRes.access_token, 'ทดสอบ Line Notification');
             console.log('========lineRes======');
             console.log(lineRes);
             console.log('=====================');
-            if (lineRes.status === 200) {
+            if (lineRes.status == 200) {
                 res.json({
                     success: true,
                     msg: 'notification is sended!'
