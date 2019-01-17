c29d005622f27c811323d0684884405e9358f3b9
kanomdook <kanung.2535@gmail.com>
2019-01-17 09:21:29 +0700

    [add] header origin

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const express = require('express');
 const http = require('http');
 const work = require('./work-flow');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+app.use(cors({ origin: '*' }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
